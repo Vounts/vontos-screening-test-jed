8396119fc324d4397704c200d109d8a3c0b8e3ba
Vounts <jheeddi2@gmail.com>
2025-06-11 06:31:45 +0800

    back to fs server

--- lib/i18n/server.ts
+++ lib/i18n/server.ts
@@ -1,19 +1,20 @@
 import i18next from "i18next";
-import HttpBackend from "i18next-http-backend";
+import Backend from "i18next-fs-backend";
+import path from "path";
 
 let instance: typeof i18next | null = null;
 
 export async function getI18n(locale: string) {
   if (!instance) {
     instance = i18next.createInstance();
-    await instance.use(HttpBackend).init({
+    await instance.use(Backend).init({
       lng: locale,
       fallbackLng: "en",
       ns: ["common"],
       defaultNS: "common",
       interpolation: { escapeValue: false },
       backend: {
-        loadPath: "/locales/{{lng}}/{{ns}}.json", // served from /public
+        loadPath: path.resolve("./public/locales/{{lng}}/{{ns}}.json"),
       },
     });
   } else {
