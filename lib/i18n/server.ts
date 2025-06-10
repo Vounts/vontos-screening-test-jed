547e1c7a48a38745979b2cc7e98cdd3dcf44d787
Vounts <jheeddi2@gmail.com>
2025-06-11 06:18:59 +0800

    test

--- lib/i18n/server.ts
+++ lib/i18n/server.ts
@@ -1,5 +1,6 @@
 import i18next from "i18next";
 import Backend from "i18next-fs-backend";
+import path from "path";
 
 let instance: typeof i18next | null = null;
 
@@ -13,7 +14,7 @@ export async function getI18n(locale: string) {
       defaultNS: "common",
       interpolation: { escapeValue: false },
       backend: {
-        loadPath: "/locales/{{lng}}/{{ns}}.json",
+        loadPath: path.resolve("./public/locales/{{lng}}/{{ns}}.json"),
       },
     });
   } else {
