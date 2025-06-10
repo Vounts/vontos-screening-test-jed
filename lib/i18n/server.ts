d33c7a6ec10145614df5213cb857782c8f8202c6
Vounts <jheeddi2@gmail.com>
2025-06-11 06:27:30 +0800

    a

--- lib/i18n/server.ts
+++ lib/i18n/server.ts
@@ -1,20 +1,19 @@
 import i18next from "i18next";
-import Backend from "i18next-fs-backend";
-import path from "path";
+import HttpBackend from "i18next-http-backend";
 
 let instance: typeof i18next | null = null;
 
 export async function getI18n(locale: string) {
   if (!instance) {
     instance = i18next.createInstance();
-    await instance.use(Backend).init({
+    await instance.use(HttpBackend).init({
       lng: locale,
       fallbackLng: "en",
       ns: ["common"],
       defaultNS: "common",
       interpolation: { escapeValue: false },
       backend: {
-        loadPath: path.resolve("./public/locales/{{lng}}/{{ns}}.json"),
+        loadPath: "/locales/{{lng}}/{{ns}}.json", // served from /public
       },
     });
   } else {
