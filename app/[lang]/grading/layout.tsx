240abb507c515d7a0b841203ce1c2c929ca5d0c0
Vounts <jheeddi2@gmail.com>
2025-06-12 04:58:24 +0800

    clean up

--- app/[lang]/grading/layout.tsx
+++
@@ -1,6 +0,0 @@
-import Layout from "@/components/Layout";
-import type { ReactNode } from "react";
-
-export default function GradingLayout({ children }: { children: ReactNode }) {
-  return <Layout>{children}</Layout>;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { baseMetaData } from "@/config/metadata";
+import Layout from "@/components/Layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
